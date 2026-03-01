8443f5fcea94c2ab32e15d493951ca58b64571d3
Rogan Palanisamy <p.roganpalanisamy1012@gmail.com>
2026-03-01 11:59:18 +0530

    Update allUniqueCharacters.js

--- allUniqueCharacters.js
+++ allUniqueCharacters.js
@@ -24,14 +24,23 @@
 */
 
 function allUniqueCharacters(s) {
-  //Place your solution here:
-  //return false;
+  const seen = new Set();
+
+  for (let char of s) {
+    if (seen.has(char)) {
+      return false;
+    }
+    seen.add(char);
+  }
+
+  return true;
 }
 
-// Example usage:
+
 console.log(allUniqueCharacters("abcdef")); // Expected output: true
 console.log(allUniqueCharacters("hello")); // Expected output: false
 console.log(allUniqueCharacters("1234567890")); // Expected output: true
 console.log(allUniqueCharacters("112233")); // Expected output: false
 console.log(allUniqueCharacters("")); // Expected output: true
 console.log(allUniqueCharacters("AaBbCc")); // Expected output: true
+    
