51eb50b56b0ae224b04f7bd9e883165f5e91af41
Rogan Palanisamy <p.roganpalanisamy1012@gmail.com>
2026-03-01 12:37:22 +0530

    Update treasureMap.js

--- treasureMap.js
+++ treasureMap.js
@@ -10,40 +10,38 @@ It must return an object with this structure:
 }
 */
 
+
+
+
 const treasureMap = function (grid) {
-  // Place your solution here
+  let treasureCount = 0;
+  let coordinates = [];
+
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      if (grid[y][x] === "X") {
+        treasureCount++;
+        coordinates.push([x, y]);
+      }
+    }
+  }
+
+  return {
+    treasureCount,
+    coordinates
+  };
 };
 
-const map1 = [
-  ["O", "O", "X", "O"],
-  ["O", "X", "O", "O"],
-  ["O", "O", "O", "X"],
+const grid = [
+  ['.', 'X', '.', '.', '.'],
+  ['.', '.', 'X', '.', '.'],
+  ['X', '.', '.', 'X', '.'],
+  ['.', '.', '.', '.', '.'],
+  ['.', 'X', '.', '.', 'X']
 ];
 
-console.log(treasureMap(map1));
-// Output:
-// {
-//   treasureCount: 3,
-//   coordinates: [ [2, 0], [1, 1], [3, 2] ]
-// }
 
-const map2 = [
-  ["O", "O"],
-  ["O", "O"],
-];
-console.log(treasureMap(map2));
-// { treasureCount: 0, coordinates: [] }
+console.log(treasureMap(grid))
 
-const map3 = [
-  ["X", "O", "O"],
-  ["O", "O", "O"],
-  ["X", "X", "O"],
-  ["O", "O", "X"],
-];
-console.log(treasureMap(map3));
-// {
-//   treasureCount: 4,
-//   coordinates: [ [0, 0], [0, 2], [1, 2], [2, 3] ]
-// }
 
 module.exports = treasureMap;
